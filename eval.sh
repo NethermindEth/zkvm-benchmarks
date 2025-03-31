07750dc2951deca65dfc86a7ba5a2f191f53cd2b
Matteo Lisotto <matteo.lisotto@gmail.com>
2025-03-31 14:57:39 +0200

    chore(deps): update to risc0 v2

--- eval.sh
+++ eval.sh
@@ -51,11 +51,11 @@ if [ "$2" == "risc0" ]; then
     # Use the risc0 toolchain.
     ATOMIC_PARAM=$(check_rust_version "risc0")
     CC=gcc CC_riscv32im_risc0_zkvm_elf=~/.risc0/cpp/bin/riscv32-unknown-elf-gcc\
-      RUSTFLAGS="-C passes=$ATOMIC_PARAM -C link-arg=-Ttext=0x00200800 -C panic=abort"\
+      RUSTFLAGS="-C passes=$ATOMIC_PARAM -C link-arg=-Ttext=0x0000200800 -C panic=abort"\
       RISC0_FEATURE_bigint2=1\
       RUSTUP_TOOLCHAIN=risc0 \
       CARGO_BUILD_TARGET=riscv32im-risc0-zkvm-elf \
-      cargo build --release --ignore-rust-version --features $2
+      cargo build --release --features $2
 
 fi
 # If the prover is sp1, then build the program.
