04cd9bd03b6cc7afec277f2c75872fa5ac8b7a6c
Matteo Lisotto <matteo.lisotto@gmail.com>
2025-06-18 16:37:18 +0200

    fix(eval.sh): add `getrandom_backend` compilation flag for risc0

--- eval.sh
+++ eval.sh
@@ -88,7 +88,11 @@ echo "Building program"
       # Use the risc0 toolchain.
       ATOMIC_PARAM=$(check_rust_version "risc0")
       CC_riscv32im_risc0_zkvm_elf=~/.risc0/cpp/bin/riscv32-unknown-elf-gcc \
-        RUSTFLAGS="-C passes=$ATOMIC_PARAM -C link-arg=-Ttext=0x00200800 -C link-arg=--fatal-warnings -C panic=abort"\
+        RUSTFLAGS="-C passes=$ATOMIC_PARAM \
+                   -C link-arg=-Ttext=0x00200800 \
+                   -C link-arg=--fatal-warnings \
+                   -C panic=abort \
+                   --cfg getrandom_backend=\"custom\"" \
         RISC0_FEATURE_bigint2=1\
         RUSTUP_TOOLCHAIN=risc0 \
         CARGO_BUILD_TARGET=riscv32im-risc0-zkvm-elf \
